75cd1eece1af278c91414ba18d8758e47db1793d
maytanan <maytanan.h@gmail.com>
2024-08-11 01:03:49 +0700

    befor big delete git add -Agit add -A

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -3,6 +3,8 @@ var { drawConnectors, drawLandmarks } = require("@mediapipe/drawing_utils");
 var { Pose, POSE_CONNECTIONS, POSE_LANDMARKS } = require("@mediapipe/pose");
 var { ipcRenderer } = require("electron");
 
+var { runGameFrame, setCanvasCtx } = require("../../pages/game/games.js");
+
 const poseLMS = {
 	NOSE: POSE_LANDMARKS.NOSE,
 	RIGHT_EYE_INNER: POSE_LANDMARKS.LEFT_EYE_INNER,
@@ -35,282 +37,286 @@ const videoElement = document.getElementsByClassName("input_video")[0] as HTMLVi
 const canvasElement = document.getElementsByClassName("output_canvas")[0] as HTMLCanvasElement;
 const canvasCtx = canvasElement.getContext("2d") as CanvasRenderingContext2D;
 
-const width = 1080;
-const height = 720;
+setCanvasCtx(canvasCtx);
 
-function calculate_angle(A: number[], B: number[], C: number[]) {
-	var AB = Math.sqrt(Math.pow(B[0] - A[0], 2) + Math.pow(B[1] - A[1], 2));
-	var BC = Math.sqrt(Math.pow(B[0] - C[0], 2) + Math.pow(B[1] - C[1], 2));
-	var AC = Math.sqrt(Math.pow(C[0] - A[0], 2) + Math.pow(C[1] - A[1], 2));
-	return Math.acos((BC * BC + AB * AB - AC * AC) / (2 * BC * AB)) * (180 / Math.PI);
-}
+// const width = 1080;
+// const height = 720;
 
-function coordinatesTouching(coordinates: number[], [x, y]: number[], error: number[] = [50, 50]) {
-	return (
-		coordinates[0] > x - error[0] &&
-		coordinates[0] < x + error[0] &&
-		coordinates[1] > y - error[1] &&
-		coordinates[1] < y + error[1]
-	);
-}
+// function calculate_angle(A: number[], B: number[], C: number[]) {
+// 	var AB = Math.sqrt(Math.pow(B[0] - A[0], 2) + Math.pow(B[1] - A[1], 2));
+// 	var BC = Math.sqrt(Math.pow(B[0] - C[0], 2) + Math.pow(B[1] - C[1], 2));
+// 	var AC = Math.sqrt(Math.pow(C[0] - A[0], 2) + Math.pow(C[1] - A[1], 2));
+// 	return Math.acos((BC * BC + AB * AB - AC * AC) / (2 * BC * AB)) * (180 / Math.PI);
+// }
 
-function drawStar(
-	cx: number,
-	cy: number,
-	outerRadius: number,
-	fill: boolean = true,
-	color: string = "yellow",
-	thickness: number = 1,
-	offset: number[] = [0, 0]
-) {
-	let ctx = canvasCtx;
-	let rot = (Math.PI / 2) * 3;
-	let [x, y] = [cx + offset[0], cy + offset[1]];
-	let spikes = 5;
-	let step = Math.PI / spikes;
-	let innerRadius = outerRadius / 2.5;
+// function coordinatesTouching(coordinates: number[], [x, y]: number[], error: number[] = [50, 50]) {
+// 	return (
+// 		coordinates[0] > x - error[0] &&
+// 		coordinates[0] < x + error[0] &&
+// 		coordinates[1] > y - error[1] &&
+// 		coordinates[1] < y + error[1]
+// 	);
+// }
 
-	ctx.beginPath();
-	ctx.moveTo(cx, cy - outerRadius);
-	for (let i = 0; i < spikes; i++) {
-		x = cx + Math.cos(rot) * outerRadius;
-		y = cy + Math.sin(rot) * outerRadius;
-		ctx.lineTo(x, y);
-		rot += step;
+// function drawStar(
+// 	cx: number,
+// 	cy: number,
+// 	outerRadius: number,
+// 	fill: boolean = true,
+// 	color: string = "yellow",
+// 	thickness: number = 1,
+// 	offset: number[] = [0, 0]
+// ) {
+// 	let ctx = canvasCtx;
+// 	let rot = (Math.PI / 2) * 3;
+// 	let [x, y] = [cx + offset[0], cy + offset[1]];
+// 	let spikes = 5;
+// 	let step = Math.PI / spikes;
+// 	let innerRadius = outerRadius / 2.5;
 
-		x = cx + Math.cos(rot) * innerRadius;
-		y = cy + Math.sin(rot) * innerRadius;
-		ctx.lineTo(x, y);
-		rot += step;
-	}
-	ctx.lineTo(cx, cy - outerRadius);
-	ctx.closePath();
+// 	ctx.beginPath();
+// 	ctx.moveTo(cx, cy - outerRadius);
+// 	for (let i = 0; i < spikes; i++) {
+// 		x = cx + Math.cos(rot) * outerRadius;
+// 		y = cy + Math.sin(rot) * outerRadius;
+// 		ctx.lineTo(x, y);
+// 		rot += step;
 
-	if (fill) {
-		ctx.fillStyle = color;
-		ctx.fill();
-	} else {
-		ctx.strokeStyle = color;
-		ctx.lineWidth = thickness;
-		ctx.stroke();
-	}
-}
+// 		x = cx + Math.cos(rot) * innerRadius;
+// 		y = cy + Math.sin(rot) * innerRadius;
+// 		ctx.lineTo(x, y);
+// 		rot += step;
+// 	}
+// 	ctx.lineTo(cx, cy - outerRadius);
+// 	ctx.closePath();
 
-class Star {
-	public activeTime: number = 0;
-	public startActiveTime: number = NaN;
-	public previouslyActive: boolean = false;
-	constructor(
-		public x: number,
-		public y: number,
-		public functionToCheckActive: () => boolean = () => false,
-		public functionToCheckCoordinates: () => number[] = () => [x, y],
-		public hasPermanentlyActiveTimer: boolean = false,
-		public active: boolean = false,
-		public color: string = "yellow",
-		public permanentlyActive: boolean = false,
-		public timeToTriggerPermanentlyActive: number = 10
-	) {
-		this.x = x;
-		this.y = y;
-		this.active = active;
-		this.color = color;
-	}
+// 	if (fill) {
+// 		ctx.fillStyle = color;
+// 		ctx.fill();
+// 	} else {
+// 		ctx.strokeStyle = color;
+// 		ctx.lineWidth = thickness;
+// 		ctx.stroke();
+// 	}
+// }
 
-	draw([x, y]: number[] = [this.x, this.y]) {
-		if (x !== this.x || y !== this.y) {
-			this.x = x;
-			this.y = y;
-		}
-		drawStar(x, y, 30, this.permanentlyActive || this.active, this.color, 5);
-	}
+// class Star {
+// 	public activeTime: number = 0;
+// 	public startActiveTime: number = NaN;
+// 	public previouslyActive: boolean = false;
+// 	constructor(
+// 		public x: number,
+// 		public y: number,
+// 		public functionToCheckActive: () => boolean = () => false,
+// 		public functionToCheckCoordinates: () => number[] = () => [x, y],
+// 		public hasPermanentlyActiveTimer: boolean = false,
+// 		public active: boolean = false,
+// 		public color: string = "yellow",
+// 		public permanentlyActive: boolean = false,
+// 		public timeToTriggerPermanentlyActive: number = 10
+// 	) {
+// 		this.x = x;
+// 		this.y = y;
+// 		this.active = active;
+// 		this.color = color;
+// 	}
 
-	isTouchingCoordinates(coordinates: number[], error: number[] = [50, 50]) {
-		return (
-			coordinates[0] > this.x - error[0] &&
-			coordinates[0] < this.x + error[0] &&
-			coordinates[1] > this.y - error[1] &&
-			coordinates[1] < this.y + error[1]
-		);
-	}
+// 	draw([x, y]: number[] = [this.x, this.y]) {
+// 		if (x !== this.x || y !== this.y) {
+// 			this.x = x;
+// 			this.y = y;
+// 		}
+// 		drawStar(x, y, 30, this.permanentlyActive || this.active, this.color, 5);
+// 	}
 
-	runAll() {
-		[this.x, this.y] = this.functionToCheckCoordinates();
-		this.active = this.functionToCheckActive();
-		if (this.hasPermanentlyActiveTimer) {
-			this.runPermanentlyActiveTimer();
-			if (this.active) {
-				this.drawTimer(this.activeTime);
-			}
-		}
-		this.draw();
-	}
+// 	isTouchingCoordinates(coordinates: number[], error: number[] = [50, 50]) {
+// 		return (
+// 			coordinates[0] > this.x - error[0] &&
+// 			coordinates[0] < this.x + error[0] &&
+// 			coordinates[1] > this.y - error[1] &&
+// 			coordinates[1] < this.y + error[1]
+// 		);
+// 	}
 
-	runPermanentlyActiveTimer() {
-		if (this.active) {
-			this.activeTime = Date.now() / 1000 - this.startActiveTime;
-		}
-		if (this.active && !this.previouslyActive) {
-			this.startActiveTime = Date.now() / 1000;
-			this.previouslyActive = true;
-		}
-		if (!this.active) {
-			this.previouslyActive = false;
-			this.activeTime = 0;
-		}
-		if (
-			this.previouslyActive &&
-			Date.now() / 1000 - this.startActiveTime > this.timeToTriggerPermanentlyActive
-		) {
-			this.permanentlyActive = true;
-		}
-	}
+// 	runAll() {
+// 		[this.x, this.y] = this.functionToCheckCoordinates();
+// 		this.active = this.functionToCheckActive();
+// 		if (this.hasPermanentlyActiveTimer) {
+// 			this.runPermanentlyActiveTimer();
+// 			if (this.active) {
+// 				this.drawTimer(this.activeTime);
+// 			}
+// 		}
+// 		this.draw();
+// 	}
 
-	drawTimer(time: number) {
-		if (!this.permanentlyActive) {
-			let timerWidth = 400;
-			let timerHeight = 40;
-			let timerX = 0;
-			let timerY = 0;
-			canvasCtx.fillStyle = "black";
-			canvasCtx.fillRect(timerX, timerY, timerWidth, timerHeight);
+// 	runPermanentlyActiveTimer() {
+// 		if (this.active) {
+// 			this.activeTime = Date.now() / 1000 - this.startActiveTime;
+// 		}
+// 		if (this.active && !this.previouslyActive) {
+// 			this.startActiveTime = Date.now() / 1000;
+// 			this.previouslyActive = true;
+// 		}
+// 		if (!this.active) {
+// 			this.previouslyActive = false;
+// 			this.activeTime = 0;
+// 		}
+// 		if (
+// 			this.previouslyActive &&
+// 			Date.now() / 1000 - this.startActiveTime > this.timeToTriggerPermanentlyActive
+// 		) {
+// 			this.permanentlyActive = true;
+// 		}
+// 	}
 
-			let timerProgress = time / this.timeToTriggerPermanentlyActive;
-			canvasCtx.fillStyle = "green";
-			canvasCtx.fillRect(timerX, timerY, timerProgress * timerWidth, timerHeight);
-		}
-	}
-}
+// 	drawTimer(time: number) {
+// 		if (!this.permanentlyActive) {
+// 			let timerWidth = 400;
+// 			let timerHeight = 40;
+// 			let timerX = 0;
+// 			let timerY = 0;
+// 			canvasCtx.fillStyle = "black";
+// 			canvasCtx.fillRect(timerX, timerY, timerWidth, timerHeight);
 
-let poseCoordinates: number[][] = [];
-let defaultPoseCoordinate = [-1000, -1000];
-let wristsMiddlePoint = defaultPoseCoordinate;
-let right_wrist = defaultPoseCoordinate;
-let left_wrist = defaultPoseCoordinate;
-let left_arm_angle = -1000;
-let right_arm_angle = -1000;
-let left_elbow = defaultPoseCoordinate;
-let right_elbow = defaultPoseCoordinate;
-let left_shoulder = defaultPoseCoordinate;
-let right_shoulder = defaultPoseCoordinate;
+// 			let timerProgress = time / this.timeToTriggerPermanentlyActive;
+// 			canvasCtx.fillStyle = "green";
+// 			canvasCtx.fillRect(timerX, timerY, timerProgress * timerWidth, timerHeight);
+// 		}
+// 	}
+// }
 
-let topStar = new Star(
-	width / 2,
-	120,
-	() => {
-		return topStar.isTouchingCoordinates(wristsMiddlePoint);
-	},
-	undefined,
-	true
-) as Star;
+// let poseCoordinates: number[][] = [];
+// let defaultPoseCoordinate = [-1000, -1000];
+// let wristsMiddlePoint = defaultPoseCoordinate;
+// let right_wrist = defaultPoseCoordinate;
+// let left_wrist = defaultPoseCoordinate;
+// let left_arm_angle = -1000;
+// let right_arm_angle = -1000;
+// let left_elbow = defaultPoseCoordinate;
+// let right_elbow = defaultPoseCoordinate;
+// let left_shoulder = defaultPoseCoordinate;
+// let right_shoulder = defaultPoseCoordinate;
 
-let sidStarCoordinates = [220, 240];
+// let topStar = new Star(
+// 	width / 2,
+// 	120,
+// 	() => {
+// 		return topStar.isTouchingCoordinates(wristsMiddlePoint);
+// 	},
+// 	undefined,
+// 	true
+// ) as Star;
 
-let pose1LeftStar = new Star(
-	sidStarCoordinates[0],
-	sidStarCoordinates[1],
-	() => {
-		return pose1LeftStar.isTouchingCoordinates(wristsMiddlePoint);
-	},
-	undefined,
-	true
-) as Star;
+// let sidStarCoordinates = [220, 240];
 
-let pose1RightStar = new Star(
-	width - sidStarCoordinates[0],
-	sidStarCoordinates[1],
-	() => {
-		return pose1RightStar.isTouchingCoordinates(wristsMiddlePoint);
-	},
-	undefined,
-	true
-) as Star;
+// let pose1LeftStar = new Star(
+// 	sidStarCoordinates[0],
+// 	sidStarCoordinates[1],
+// 	() => {
+// 		return pose1LeftStar.isTouchingCoordinates(wristsMiddlePoint);
+// 	},
+// 	undefined,
+// 	true
+// ) as Star;
 
-let angleMin = 150;
-let angleMax = 200;
+// let pose1RightStar = new Star(
+// 	width - sidStarCoordinates[0],
+// 	sidStarCoordinates[1],
+// 	() => {
+// 		return pose1RightStar.isTouchingCoordinates(wristsMiddlePoint);
+// 	},
+// 	undefined,
+// 	true
+// ) as Star;
 
-let pose1LeftElbowStar = new Star(
-	left_elbow[0],
-	left_elbow[1],
-	() => {
-		return left_arm_angle > angleMin && left_arm_angle < angleMax;
-	},
-	() => {
-		return left_elbow;
-	}
-) as Star;
+// let angleMin = 150;
+// let angleMax = 200;
 
-let pose1RightElbowStar = new Star(
-	right_elbow[0],
-	right_elbow[1],
-	() => {
-		return right_arm_angle > angleMin && right_arm_angle < angleMax;
-	},
-	() => {
-		return right_elbow;
-	}
-) as Star;
+// let pose1LeftElbowStar = new Star(
+// 	left_elbow[0],
+// 	left_elbow[1],
+// 	() => {
+// 		return left_arm_angle > angleMin && left_arm_angle < angleMax;
+// 	},
+// 	() => {
+// 		return left_elbow;
+// 	}
+// ) as Star;
 
-let wristsMiddleStar = new Star(
-	wristsMiddlePoint[0],
-	wristsMiddlePoint[1],
-	() => {
-		return coordinatesTouching(left_wrist, right_wrist, [300, 200]);
-	},
-	() => {
-		return wristsMiddlePoint;
-	}
-) as Star;
+// let pose1RightElbowStar = new Star(
+// 	right_elbow[0],
+// 	right_elbow[1],
+// 	() => {
+// 		return right_arm_angle > angleMin && right_arm_angle < angleMax;
+// 	},
+// 	() => {
+// 		return right_elbow;
+// 	}
+// ) as Star;
 
-let pose1stars = [
-	topStar,
-	pose1LeftStar,
-	pose1RightStar,
-	pose1LeftElbowStar,
-	pose1RightElbowStar,
-	wristsMiddleStar,
-];
+// let wristsMiddleStar = new Star(
+// 	wristsMiddlePoint[0],
+// 	wristsMiddlePoint[1],
+// 	() => {
+// 		return coordinatesTouching(left_wrist, right_wrist, [300, 200]);
+// 	},
+// 	() => {
+// 		return wristsMiddlePoint;
+// 	}
+// ) as Star;
+
+// let pose1stars = [
+// 	topStar,
+// 	pose1LeftStar,
+// 	pose1RightStar,
+// 	pose1LeftElbowStar,
+// 	pose1RightElbowStar,
+// 	wristsMiddleStar,
+// ];
 
 function onResults(results: any) {
-	canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+	runGameFrame(results);
+	/*
+	// canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
 
-	canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+	// canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
 
-	if (!results.poseLandmarks) {
-		return;
-	}
+	// if (!results.poseLandmarks) {
+	// 	return;
+	// }
 
-	drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
-		color: "#00FF00",
-		lineWidth: 4,
-	});
-	drawLandmarks(canvasCtx, results.poseLandmarks, { color: "#FF0000", lineWidth: 2 });
+	// drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
+	// 	color: "#00FF00",
+	// 	lineWidth: 4,
+	// });
+	// drawLandmarks(canvasCtx, results.poseLandmarks, { color: "#FF0000", lineWidth: 2 });
 
-	poseCoordinates = [];
-	results.poseLandmarks.forEach((landmark: any) => {
-		poseCoordinates.push([landmark.x * width, landmark.y * height]);
-	});
+	// poseCoordinates = [];
+	// results.poseLandmarks.forEach((landmark: any) => {
+	// 	poseCoordinates.push([landmark.x * width, landmark.y * height]);
+	// });
 
-	left_wrist = poseCoordinates[poseLMS.LEFT_WRIST];
-	right_wrist = poseCoordinates[poseLMS.RIGHT_WRIST];
+	// left_wrist = poseCoordinates[poseLMS.LEFT_WRIST];
+	// right_wrist = poseCoordinates[poseLMS.RIGHT_WRIST];
 
-	left_elbow = poseCoordinates[poseLMS.LEFT_ELBOW];
-	right_elbow = poseCoordinates[poseLMS.RIGHT_ELBOW];
+	// left_elbow = poseCoordinates[poseLMS.LEFT_ELBOW];
+	// right_elbow = poseCoordinates[poseLMS.RIGHT_ELBOW];
 
-	left_shoulder = poseCoordinates[poseLMS.LEFT_SHOULDER];
-	right_shoulder = poseCoordinates[poseLMS.RIGHT_SHOULDER];
+	// left_shoulder = poseCoordinates[poseLMS.LEFT_SHOULDER];
+	// right_shoulder = poseCoordinates[poseLMS.RIGHT_SHOULDER];
 
-	left_arm_angle = calculate_angle(left_shoulder, left_elbow, left_wrist);
-	right_arm_angle = calculate_angle(right_shoulder, right_elbow, right_wrist);
+	// left_arm_angle = calculate_angle(left_shoulder, left_elbow, left_wrist);
+	// right_arm_angle = calculate_angle(right_shoulder, right_elbow, right_wrist);
 
-	wristsMiddlePoint = [
-		(left_wrist[0] + right_wrist[0]) / 2,
-		(left_wrist[1] + right_wrist[1]) / 2,
-	];
+	// wristsMiddlePoint = [
+	// 	(left_wrist[0] + right_wrist[0]) / 2,
+	// 	(left_wrist[1] + right_wrist[1]) / 2,
+	// ];
 
-	pose1stars.forEach((star) => {
-		star.runAll();
-	});
+	// pose1stars.forEach((star) => {
+	// 	star.runAll();
+	// });*/
 }
 
 const pose = new Pose({
@@ -332,7 +338,7 @@ pose.onResults(onResults);
 
 let stream: MediaStream | null = null;
 
-async function setupWebcam() {
+async function startWebcam() {
 	try {
 		stream = await navigator.mediaDevices.getUserMedia({
 			video: {
@@ -364,7 +370,7 @@ function stopWebcam() {
 }
 
 ipcRenderer.on("start-web-game", () => {
-	setupWebcam();
+	startWebcam();
 	document.getElementById("listgamehidden")?.classList.remove("hidden");
 });
 
